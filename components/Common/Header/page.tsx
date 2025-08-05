4b4c6f6f7f314f3cc10dfecee0b041cfd221447f
tantn09 <tantn09@gmail.com>
2025-08-06 02:28:15 +0700

    [HomePage] v1

--- components/Common/Header/page.tsx
+++ components/Common/Header/page.tsx
@@ -1,19 +1,34 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{backgroundColor: scrolled ? "#000000e6" : "transparent"}}>
       <div className={styles.headerContainer}>
         <div className={styles.logo}>
           <Image
-            src="/images/logo.png"
+            src="/images/logo.svg"
             alt="Logo"
             width={80}
             height={80}
             quality={100}
             priority
           />
+          <div>HZET GLOBAL</div>
         </div>
         <div className={styles.navBar}>
             <div className={styles.navBarItem}>Giới Thiệu</div>
