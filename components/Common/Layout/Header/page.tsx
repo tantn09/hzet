699c9394823999377839faf915cc8fab3f83d11e
tantn09 <tantn09@gmail.com>
2025-09-12 13:34:19 +0700

    fig bug

--- components/Common/Layout/Header/page.tsx
+++ components/Common/Layout/Header/page.tsx
@@ -25,7 +25,7 @@ export default function Header() {
       <div className={styles.headerContainer}>
         <div className={styles.logo} onClick={() => router.push("/")}>
           <Image
-            src="/images/logo1.jpg"
+            src="/images/logo.svg"
             className={styles.logoImg}
             alt="Logo"
             width={80}
